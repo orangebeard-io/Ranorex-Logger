45c61360fa3dfa37b2d553393eec74f9fa097482
Tcnh <tom.heintzberger@gmail.com>
2020-09-29 11:22:42 +0200

    update gitignore

--- Requests/UpdateOrangebeardLaunchRequest.cs
+++ Requests/UpdateOrangebeardLaunchRequest.cs
@@ -1,4 +1,14 @@
-﻿using ReportPortal.Client.Abstractions.Models;
+﻿/*
+ * Copyright 2020 Orangebeard.io (https://www.orangebeard.io)
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using ReportPortal.Client.Abstractions.Models;
 using ReportPortal.Client.Abstractions.Requests;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
