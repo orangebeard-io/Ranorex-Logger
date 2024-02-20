14e17730502835f8b42ee04cae32efbee746de0e
tcnh <tom@orangebeard.io>
2024-02-20 17:00:22 +0100

    Truncate long descriptions; replace empty stepnames; latest client

--- RunContext/ItemCreationData.cs
+++ RunContext/ItemCreationData.cs
@@ -1,15 +1,29 @@
-﻿using Orangebeard.Client.V3.Entity;
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace RanorexOrangebeardListener.RunContext
 {
     public class ItemCreationData
     {
-        public DateTime StartTime;
-        public string Type;
-        public string Name;
-        public string Description;
-        public ISet<Orangebeard.Client.V3.Entity.Attribute> Attributes;
+        private string _name;
+        private string _description;
+
+        public DateTime StartTime { get; set; }
+        public string Type { get; set; }
+
+        public string Name
+        {
+            get => string.IsNullOrEmpty(_name) ? "NO_NAME" : _name;
+            set => _name = value;
+        }
+
+        public string Description
+        {
+            get => _description;
+            set => _description = value?.Length > 1024 ? value.Substring(0, 1021) + "..." : value;
+        }
+
+        public ISet<Orangebeard.Client.V3.Entity.Attribute> Attributes { get; set; }
     }
+    
 }
